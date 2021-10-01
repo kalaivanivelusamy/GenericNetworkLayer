ad1a53acdc3636807a153fc4099b33ad47c72c1a
KalaivaniVelusamy <kalai.v.vani@gmail.com>
2021-10-01 12:02:48 +0530

    initial commit

---
+++ GenericNetworkLayer/GenericNetworkLayer/GenericNetworkLayerApp.swift
@@ -0,0 +1,16 @@
+//
+//  GenericNetworkLayerApp.swift
+//  GenericNetworkLayer
+//
+//
+
+import SwiftUI
+
+@main
+struct GenericNetworkLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GenericNetworkLayer/GenericNetworkLayerUITests/GenericNetworkLayerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GenericNetworkLayerUITestsLaunchTests.swift
+//  GenericNetworkLayerUITests
+//
+//  Created by V, Kalaivani V. (623-Extern) on 01/10/21.
+//
+
+import XCTest
+
+class GenericNetworkLayerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
