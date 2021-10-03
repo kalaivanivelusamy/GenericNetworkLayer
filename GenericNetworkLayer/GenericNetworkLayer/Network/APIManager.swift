58b1fdf64a918af3a9dea6385bffa7780380501e
KalaivaniVelusamy <kalai.v.vani@gmail.com>
2021-10-03 21:34:30 +0530

    added api methods to set headers and encoded parameters for get and post requests

--- GenericNetworkLayer/GenericNetworkLayer/Network/APIManager.swift
+++ GenericNetworkLayer/GenericNetworkLayer/Network/APIManager.swift
@@ -22,6 +22,9 @@ enum HttpMethod: String {
     case delete = "DELETE"
 }
 
+typealias Parameters = [String : Any]
+
+
 enum URLEncoding {
     case queryString
     case none
@@ -44,7 +47,58 @@ enum URLEncoding {
     }
 }
 
-typealias Parameters = [String : Any]
+enum APIManager {
+    
+    case getAPI(path: String, data: Parameters) 
+    case postAPI(path: String, data: Parameters)
+    
+    var method: HttpMethod{
+        switch self {
+            case .getAPI:
+                return .get
+            case .postAPI:
+                return .post
+        }
+    }
+    
+    var path: String {
+        switch self {
+            case .getAPI(let path, _):
+                return path
+            case .postAPI(let path, _):
+                return path
+        }
+    }
+    
+    static var baseURL: URL = URL(string: "https://jsonplaceholder.typicode.com/")!
+
+    func addHeaders(request: inout URLRequest){
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    }
+    
+    func asURLRequest() -> URLRequest {
+        var request = URLRequest(url: Self.baseURL.appendingPathComponent(path))
+        request.httpMethod = method.rawValue
+        
+        var parameters = Parameters()
+        switch self {
+            case .getAPI(_, let queries):
+                queries.forEach({parameters[$0] = $1})
+                URLEncoding.queryString.encode(&request, with: parameters)
+            case .postAPI(_, let queries): 
+                queries.forEach({parameters[$0] = $1})
+                if let jsonData = try? JSONSerialization.data(withJSONObject: parameters){
+                    request.httpBody = jsonData
+                }
+            }
+        self.addHeaders(request: &request)
+        
+        return request
+    }
+    
+    
+}
+
 
 
 
